a03f2a06fdae7e2c1c48d51ee2e3657b06b6a7c0
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-08-28 18:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string===whisper(string)){
-  return ('I can\'t hear you!');}
-  else if (string===shout(string))
-  return ('YES INDEED!');
-  else if (string==='I love you, Grandma.')
-  return ('I love you, too.');
-  else
-  return('you\'re the best grandma');
+  return ('I can\'t hear you!')}
+  else if (string===shout(string)){
+  return ('YES INDEED!')}
+  else if (string==='I love you, Grandma.'){
+  return ('I love you, too.')}
+  else{
+  return('you\'re the best grandma')}
 }
